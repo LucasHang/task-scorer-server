275dab1f1f3609076399fca933d023180f7ad1e6
Lucas Hang <lucas.hhang@gmail.com>
2022-07-04 23:46:23 -0300

    feat: rota de status

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const Gun = require('Gun');
+const express = require("express");
+const Gun = require("Gun");
 
 const app = express();
 
 app.use(Gun.serve);
 
+app.get("/", (_, res) => {
+  res.end("Tudo certo por aqui");
+});
+
 const port = 3030;
 const server = app.listen(port, () => {
-    console.info(`Gun server listening at port ${port}`);
+  console.info(`Gun server listening at port ${port}`);
 });
 
 Gun({ web: server });
